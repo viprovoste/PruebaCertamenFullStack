9f09a7ba368d9e6cff733003d976f8effc01cb3e
vicente <vi.provoste@duocuc.cl>
2026-04-09 22:48:05 -0400

    first commit

---
+++ certamen/src/main/java/com/fullstack/certamen/CertamenApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.certamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertamenApplication.class, args);
+	}
+
+}
---
+++ certamen/src/test/java/com/fullstack/certamen/CertamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.certamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
