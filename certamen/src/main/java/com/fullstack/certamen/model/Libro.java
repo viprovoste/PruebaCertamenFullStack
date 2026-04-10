d647b8d4211daef8df7c4df1e6a1db704d690226
vicente <vi.provoste@duocuc.cl>
2026-04-09 23:01:55 -0400

    libro creado

---
+++ certamen/src/main/java/com/fullstack/certamen/model/Libro.java
@@ -0,0 +1,18 @@
+package com.fullstack.certamen.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Libro {
+    
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private int fechapublicacion;
+    private String autor;
+}
